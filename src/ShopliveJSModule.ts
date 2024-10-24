87355051a6cfbb64082b6a8443616665376252f0
nguyenquocdat <nguyenquocdat.hcmus@gmail.com>
2024-10-24 17:56:02 +0700

    update version 1.0.5

--- src/ShopliveJSModule.ts
+++ src/ShopliveJSModule.ts
@@ -1,44 +1,42 @@
-import {NativeModules} from 'react-native';
-const {ShopliveModule} = NativeModules;
-import {addEventListener, removeEventListener} from './EventEmitter';
-import type {EventData} from './EventEmitter';
+import { NativeModules } from 'react-native';
+const { ShopliveModule } = NativeModules;
+import { addEventListener, removeEventListener } from './EventEmitter';
+import type { EventData } from './EventEmitter';
 
 class ShopliveJSModule {
-
   onHandleNavigation = (listener: (payload: EventData) => void) => {
     addEventListener('handleNavigation', (payload: EventData) =>
-      listener(payload),
+      listener(payload)
     );
   };
   onDownloadCoupon = (listener: (payload: EventData) => void) => {
     addEventListener('downloadCoupon', (payload: EventData) =>
-      listener(payload),
+      listener(payload)
     );
   };
-  
-  
+
   onReceivedCommand = (listener: (payload: EventData) => void) => {
     addEventListener('onReceivedCommand', (payload: EventData) =>
-      listener(payload),
+      listener(payload)
     );
-  }
+  };
 
   onError = (listener: (payload: EventData) => void) => {
     addEventListener('onError', (payload: EventData) => listener(payload));
   };
   onPlayerClosing = (listener: (payload: EventData) => void) => {
     addEventListener('playerClosing', (payload: EventData) =>
-      listener(payload),
+      listener(payload)
     );
   };
   onPlayerDestroyed = (listener: (payload: EventData) => void) => {
     addEventListener('playerDestroyed', (payload: EventData) =>
-      listener(payload),
+      listener(payload)
     );
   };
   onPlayerCreated = (listener: (payload: EventData) => void) => {
     addEventListener('playerCreated', (payload: EventData) =>
-      listener(payload),
+      listener(payload)
     );
   };
 
@@ -50,7 +48,7 @@ class ShopliveJSModule {
     removeEventListener('playerClosing');
     removeEventListener('playerDestroyed');
     removeEventListener('playerCreated');
-  }
+  };
 
   setAccessKey(accessKey: string) {
     ShopliveModule.setAccessKey(accessKey);
@@ -61,9 +59,13 @@ class ShopliveJSModule {
     ShopliveModule.play(campaignKey);
   }
 
-  setShareUrl(url:string) {
-    ShopliveModule.setShareUrl(url);
-  }  
+  playWithAuthToken(campaignKey: string, jwtToken: string) {
+    console.log('playShoplive', campaignKey);
+    ShopliveModule.playWithAuthToken(campaignKey, jwtToken);
+  }
 
+  setShareUrl(url: string) {
+    ShopliveModule.setShareUrl(url);
+  }
 }
 export default ShopliveJSModule;
